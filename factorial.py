7b03373d6984d0806cc3b2d0125f128c9e2c2744
Otar <86528840+Blazkowich@users.noreply.github.com>
2022-09-30 14:31:14 +0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
 number = int(input("Enter the number : "))
-factorial = 1
+startNum = 1
 
 for num in range(1, number + 1):
-    factorial *= num
-    print("The Factorial of %d = %d" % (number, factorial))
+    startNum = startNum * num
+print("The Factorial of %d = %d" % (number, startNum))
