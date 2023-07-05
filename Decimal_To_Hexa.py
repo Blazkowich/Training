78266efe28ca7682f0659f012ba91ccfa396190e
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-07-05 12:25:12 +0400

    Update Decimal_To_Hexa.py

--- Decimal_To_Hexa.py
+++ Decimal_To_Hexa.py
@@ -9,3 +9,19 @@ def decimal_to_hexadecimal(number):
 decimal_number = 45891
 hexadecimal_number = decimal_to_hexadecimal(decimal_number)
 print(hexadecimal_number)
+
+def hexadecimal_to_decimal(hexadecimal_number):
+    decimal = 0
+    power = 0
+    for digit in reversed(hexadecimal_number):
+        if digit.isdigit():
+            decimal += int(digit) * (16 ** power)
+        else:
+            decimal += (ord(digit.upper()) - 55) * (16 ** power)
+        power += 1
+    return decimal
+
+hexadecimal_number = "B43"
+decimal_number = hexadecimal_to_decimal(hexadecimal_number)
+print(decimal_number)
+
