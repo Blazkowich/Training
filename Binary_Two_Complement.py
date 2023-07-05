82ebe03e0e9304574b1efa7d698fb1a942c93c18
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-07-05 13:22:32 +0400

    Update Binary_Two_Complement.py

--- Binary_Two_Complement.py
+++ Binary_Two_Complement.py
@@ -1,47 +1,42 @@
 def convert(n):
+    if n >= 0:
+        return convert_positive(n)
+    else:
+        return convert_negative(n)
+
+def convert_positive(n):
     result = ""
-
-    # Check if the number is negative
-    is_negative = False
-    if n < 0:
-        is_negative = True
-        n = abs(n)
-
-    # Convert the absolute value to binary representation
+    
     while n > 0:
         result = str(n % 2) + result
         n = n // 2
 
-    # If the number is negative, invert all bits and add 1
-    if is_negative:
-        result = invert_bits(result)
-        result = add_one(result)
+    while len(result) < 8:
+        result = "0" + result
 
     return result
 
-def invert_bits(binary):
-    inverted = ""
-    for bit in binary:
+def convert_negative(n):
+    positive_binary = convert_positive(-n)
+    flipped_bits = ""
+    for bit in positive_binary:
         if bit == "0":
-            inverted += "1"
+            flipped_bits += "1"
         else:
-            inverted += "0"
-    return inverted
-
-def add_one(binary):
+            flipped_bits += "0"
+    
     result = ""
     carry = 1
-    for bit in binary[::-1]:  # Reverse the binary string
+    for bit in flipped_bits[::-1]:
         if bit == "0" and carry == 1:
             result = "1" + result
             carry = 0
         elif bit == "1" and carry == 1:
             result = "0" + result
-            carry = 1
         else:
             result = bit + result
-    if carry == 1:
-        result = "1" + result
+    
     return result
 
 print(convert(56))
+print(convert(-56))
